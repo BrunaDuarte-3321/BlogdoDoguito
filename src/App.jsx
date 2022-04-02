acae12ea0c23e76a4cf5ece712f946901388a431
BrunaDuarte-3321 <62619506+BrunaDuarte-3321@users.noreply.github.com>
2022-04-02 10:45:04 -0300

    Criamos componente 404 para quando a pessoa tentar acessar alguma página que não existe. Adicionando um navBarr utilizando React Router para siular uma MPA.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './assets/css/base/base.css';
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
+import Pagina404 from './paginas/Pagina404';
+import Cabecalho from './assets/css/componentes/Cabecalho';
 
 function App() {
   return (
     <BrowserRouter>
+      <Cabecalho />
       <Routes>
         <Route path="/sobre" element={<Sobre />} />
+        <Route path="*" element={<Pagina404 />}></Route>
         <Route index element={<Home />} />
       </Routes>
     </BrowserRouter>
