e58bfda5245836b15b1bdc102f750c775431cd0e
BrunaDuarte-3321 <62619506+BrunaDuarte-3321@users.noreply.github.com>
2022-04-02 10:14:29 -0300

    Configurção das rotas para componentes diversos com a biblioteca React Router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './assets/css/base/base.css';
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
-import './assets/css/base/base.css';
 
 function App() {
   return (
-    <>
-      <Home />
-      <Sobre />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sobre" element={<Sobre />} />
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
